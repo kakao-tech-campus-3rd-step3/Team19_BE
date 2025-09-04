6bd567cd9bae402648173cc694484d41e37dc9f6
uvrvuoon <0524yar@gmail.com>
2025-09-04 20:33:03 +0900

    feat:Review Repository 생성

---
+++ src/main/java/com/team19/musuimsa/review/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.team19.musuimsa.review.repository;
+
+import com.team19.musuimsa.review.domain.Review;
+import com.team19.musuimsa.user.domain.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByShelter(Shelter shelter);
+
+    List<Review> findByUser(User user);
+
+}
