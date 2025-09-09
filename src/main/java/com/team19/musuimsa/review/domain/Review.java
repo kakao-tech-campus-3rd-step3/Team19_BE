ea8164c8b80e93f8b32fdc37526e4fa5bb096f5e
uvrvuoon <0524yar@gmail.com>
2025-09-09 23:13:27 +0900

    refactor: dateTime 패키지명 audit로 수정

--- src/main/java/com/team19/musuimsa/review/domain/Review.java
+++ src/main/java/com/team19/musuimsa/review/domain/Review.java
@@ -1,6 +1,6 @@
 package com.team19.musuimsa.review.domain;
 
-import com.team19.musuimsa.dateTime.BaseEntity;
+import com.team19.musuimsa.audit.BaseEntity;
 import com.team19.musuimsa.review.dto.CreateReviewRequest;
 import com.team19.musuimsa.shelter.domain.Shelter;
 import com.team19.musuimsa.user.domain.User;
