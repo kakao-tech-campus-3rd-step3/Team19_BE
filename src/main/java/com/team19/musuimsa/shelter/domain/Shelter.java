79fb235d816b5d55b7403df9b22f077e8fa6b04a
hydrationn <146078205+hydrationn@users.noreply.github.com>
2025-09-18 00:18:02 +0900

    [Week6] Shelter 멘토님 피드백 반영 추가 (#19)
    * fix: 동시성 제어를 위한 @Version + 낙관적락 설정
    
    * refactor: shelter import 와일드카드 리팩토링
    
    * refactor: 필드명 명확화를 위한 리팩토링(RETRY -> MAX_RETRY)

--- src/main/java/com/team19/musuimsa/shelter/domain/Shelter.java
+++ src/main/java/com/team19/musuimsa/shelter/domain/Shelter.java
@@ -4,13 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.math.BigDecimal;
-import java.time.LocalTime;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
 
 @Entity
 @Getter
@@ -70,6 +72,9 @@ public class Shelter {
 
     @Column(length = 255)
     private String photoUrl;
+
+    @Version
+    private Long version;
 
     public void updateTotalRating(Integer totalRating) {
         this.totalRating = totalRating;
