4babd92620871697e89cccee460c6dcedd90c7c8
uvrvuoon <0524yar@gmail.com>
2025-10-05 21:13:05 +0900

    refactor: Cache 관련 설정을 application.properties로 이동

--- src/main/java/com/team19/musuimsa/config/CacheConfig.java
+++ src/main/java/com/team19/musuimsa/config/CacheConfig.java
@@ -2,6 +2,7 @@ package com.team19.musuimsa.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
@@ -12,11 +13,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfig {
 
+    @Value("${spring.cache.caffeine.expire-after-write}")
+    private String expireAfterWrite;
+
+    @Value("${spring.cache.caffeine.maximum-size}")
+    private long maximumSize;
+
     @Bean
     public CacheManager cacheManager() {
+        Duration expiryDuration = Duration.parse("PT" + expireAfterWrite.toUpperCase());
+
         Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
-                .expireAfterWrite(Duration.ofMinutes(3))
-                .maximumSize(10_000);
+                .expireAfterWrite(expiryDuration)
+                .maximumSize(maximumSize);
 
         CaffeineCacheManager manager = new CaffeineCacheManager();
         manager.setCaffeine(caffeine);
