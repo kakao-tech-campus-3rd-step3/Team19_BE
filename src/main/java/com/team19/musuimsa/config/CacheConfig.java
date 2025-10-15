ffa0947eaa9ddecbdaf20e8440a7c7251237f51a
hydrationn <suhwaparkk1031@gmail.com>
2025-10-15 13:14:54 +0900

    refactor: 무쉼사 메인페이지 캐시 설정을 위한 weather 설정 정보 명확화

--- src/main/java/com/team19/musuimsa/config/CacheConfig.java
+++ src/main/java/com/team19/musuimsa/config/CacheConfig.java
@@ -1,7 +1,6 @@
 package com.team19.musuimsa.config;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
-import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,23 +8,25 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
-    @Value("${spring.cache.caffeine.expire-after-write}")
-    private String expireAfterWrite;
+    @Value("${cache.weather.expire-after-write}")
+    private String weatherExpireAfterWrite;
 
-    @Value("${spring.cache.caffeine.maximum-size}")
-    private long maximumSize;
+    @Value("${cache.weather.maximum-size}")
+    private long weatherMaximumSize;
 
     @Bean
     public CacheManager cacheManager() {
-        Duration expiryDuration = Duration.parse("PT" + expireAfterWrite.toUpperCase());
+        Duration expiryDuration = Duration.parse("PT" + weatherExpireAfterWrite.toUpperCase());
 
         Caffeine<Object, Object> caffeine = Caffeine.newBuilder()
                 .expireAfterWrite(expiryDuration)
-                .maximumSize(maximumSize);
+                .maximumSize(weatherMaximumSize);
 
         CaffeineCacheManager manager = new CaffeineCacheManager();
         manager.setCaffeine(caffeine);
