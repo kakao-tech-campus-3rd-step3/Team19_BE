c226576fe1c5d0fa6defa09d8be154a427b3799f
Eojin Lee <win929@naver.com>
2025-10-16 22:20:10 +0900

    fix: 안쓰는 필드 삭제

--- src/main/java/com/team19/musuimsa/user/service/UserService.java
+++ src/main/java/com/team19/musuimsa/user/service/UserService.java
@@ -36,8 +36,6 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    private static final String DEFAULT_PROFILE_IMAGE_URL = "https://wikis.krsocsci.org/images/a/aa/%EA%B8%B0%EB%B3%B8_%ED%94%84%EB%A1%9C%ED%95%84.png";
-
     public Long signUp(SignUpRequest signUpRequest) {
         checkDuplicateUser(signUpRequest);
 
