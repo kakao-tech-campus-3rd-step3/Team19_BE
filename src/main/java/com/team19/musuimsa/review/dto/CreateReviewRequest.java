7f7ed195008d550b34a0ef01c4bbb686fb49a863
uvrvuoon <0524yar@gmail.com>
2025-10-29 21:52:50 +0900

    feature: 리뷰 글자 길이 100자 제한

--- src/main/java/com/team19/musuimsa/review/dto/CreateReviewRequest.java
+++ src/main/java/com/team19/musuimsa/review/dto/CreateReviewRequest.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CreateReviewRequest(
-        @NotBlank String content,
+        @NotBlank @Size(max = 100, message = "리뷰는 100자까지 작성 가능합니다.") String content,
         @NotNull @Min(1) @Max(5) int rating,
         String photoUrl
 ) {
