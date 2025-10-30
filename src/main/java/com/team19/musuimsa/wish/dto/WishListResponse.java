2d1cba7443ba34aa3a2b5a103626e9e74ddf0194
Eojin Lee <win929@naver.com>
2025-10-30 10:45:04 +0900

    feat: wish/dto Swagger 추가

--- src/main/java/com/team19/musuimsa/wish/dto/WishListResponse.java
+++ src/main/java/com/team19/musuimsa/wish/dto/WishListResponse.java
@@ -1,8 +1,12 @@
 package com.team19.musuimsa.wish.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+@Schema(description = "위시리스트 응답 데이터")
 public record WishListResponse(
+        @Schema(description = "위시리스트 아이템 목록", example = "[]")
         List<WishListItemResponse> items
 ) {
+
 }
