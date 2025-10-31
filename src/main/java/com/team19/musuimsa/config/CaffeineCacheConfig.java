79512bc04e1a29579be2f13c36a3f35c6ade7413
uvrvuoon <0524yar@gmail.com>
2025-10-31 14:30:10 +0900

    refactor: 기존 CacheConfig 파일명 CaffeineCacheConfig로 변경 (caffeine 전용 설정임을 명시)

--- src/main/java/com/team19/musuimsa/config/CaffeineCacheConfig.java
+++ src/main/java/com/team19/musuimsa/config/CaffeineCacheConfig.java
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Profile;
 @Profile({"dev", "prod"})
 @EnableCaching
 @Configuration
-public class CacheConfig {
+public class CaffeineCacheConfig {
 
     @Value("${cache.weather.expire-after-write}")
     private Duration weatherExpireAfterWrite;
