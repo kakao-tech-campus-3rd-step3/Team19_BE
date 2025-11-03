835615ddbceea8bb736b790eef1109e6f986555e
Eojin Lee <win929@naver.com>
2025-11-04 03:34:36 +0900

    feat: 배치 작업 활성화 및 테스트용 배치 시간 조정 (2시 -> 3시45분)

--- src/main/java/com/team19/musuimsa/scheduler/ShelterImportScheduler.java
+++ src/main/java/com/team19/musuimsa/scheduler/ShelterImportScheduler.java
@@ -16,7 +16,7 @@ public class ShelterImportScheduler {
     private final Job shelterImportJob;
 
     // 매일 새벽 2시에 실행
-    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 45 3 * * *")
     public void runShelterImportJob() throws Exception {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("jobID", String.valueOf(System.currentTimeMillis()))
