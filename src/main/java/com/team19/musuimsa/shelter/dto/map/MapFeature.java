e3f23c064a8e81409d340cd388f8485797ae2679
Eojin Lee <73630653+win929@users.noreply.github.com>
2025-11-07 02:16:24 +0900

    fix: 어노테이션 제거 (#146)

--- src/main/java/com/team19/musuimsa/shelter/dto/map/MapFeature.java
+++ src/main/java/com/team19/musuimsa/shelter/dto/map/MapFeature.java
@@ -1,15 +1,5 @@
 package com.team19.musuimsa.shelter.dto.map;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(
-        use = JsonTypeInfo.Id.NAME,
-        property = "@type"
-)
-@JsonSubTypes({
-        @JsonSubTypes.Type(value = ClusterFeature.class, name = "cluster"),
-        @JsonSubTypes.Type(value = MapShelterResponse.class, name = "shelter")
-})
 public sealed interface MapFeature permits ClusterFeature, MapShelterResponse {
+
 }
