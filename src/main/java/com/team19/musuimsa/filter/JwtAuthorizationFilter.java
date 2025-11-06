cdf56a9215809156e9b869768a4a8d7950b9694f
hydrationn <suhwaparkk1031@gmail.com>
2025-11-06 16:12:49 +0900

    fix: 사용자 이미지 업로드 관련 NPE 및 환경 설정 오류 수정

--- src/main/java/com/team19/musuimsa/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/team19/musuimsa/filter/JwtAuthorizationFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +16,8 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,7 +28,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-            FilterChain filterChain)
+                                 FilterChain filterChain)
             throws ServletException, IOException {
 
         String token = resolveToken(request);
@@ -55,7 +56,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         UserDetailsImpl userDetails = (UserDetailsImpl) userDetailsService.loadUserByUsername(
                 email);
-        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails.getUser(), null,
                 userDetails.getAuthorities());
         context.setAuthentication(authentication);
         SecurityContextHolder.setContext(context);
